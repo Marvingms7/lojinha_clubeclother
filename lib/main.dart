d9328d6e64636e9e694f9e653019725eb1d23b01
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2022-11-07 15:57:11 -0300

    firebase configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:lojinha_clubeclother/firebase_options.dart';
 import 'package:lojinha_clubeclother/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(const HomePage());
 }
 
